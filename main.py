658b6bf35eb6f22d0882793efd590c4a12ff9436
alexvolnoff18 <alex.volnoff@mail.ru>
2022-11-16 14:47:03 +0300

    solving the second task

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
+
+
+import os
 import requests
 
+# task 2
 TOKEN = '2619421814940190'
 urls = [
     f'https://www.superheroapi.com/api.php/{TOKEN}/search/Hulk',
     f'https://www.superheroapi.com/api.php/{TOKEN}/search/Thanos',
     f'https://www.superheroapi.com/api.php/{TOKEN}/search/Captain%America',
 ]
-
-
 def requests_get(url_all):
     resp = (requests.get(url) for url in url_all)
     return resp
@@ -31,3 +33,32 @@ def smartest():
 
 
 smartest()
+
+
+# task 2
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def upload(self, file_path: str):
+        """Метод загружает файл file_path на Яндекс.Диск"""
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        filename = file_path.split('/', )[-1]
+        headers = {'Content-Type': 'application/json', 'Authorization': 'OAuth {}'.format(self.token)}
+        params = {"path": f"{filename}", "overwrite": "true"}
+        _response = requests.get(upload_url, headers=headers, params=params).json()
+        href = _response.get("href", "")
+        responce = requests.put(href, data=open(file_path, 'rb'))
+        responce.raise_for_status()
+        if responce.status_code == 201:
+            print('Файл, успешно загружен!')
+        else:
+            print(f"Ошибка загрузки! Код ошибки: {responce.status_code}")
+
+
+if __name__ == '__main__':
+    # Получить путь к загружаемому файлу и токен от пользователя
+    path_to_file = "C:/Users/DNS/OneDrive/Desktop/new.txt"
+    token = os.getenv('TOKEN_YaD_VOLNOFF')
+    uploader = YaUploader(token)
+    uploader.upload(path_to_file)
